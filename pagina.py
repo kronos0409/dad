9a269d08a0eda990badad792ea5552a5d8f23972
kronos0409 <jaranicolas101@gmail.com>
2024-12-24 11:24:03 -0300

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -22,8 +22,8 @@ def procesar_hojas(archivo,tramo):
         patron = len(hojas_procesadas)
         patron = int(np.round(100/(patron)))
         n = 0
-        st.write(hojas_procesadas[:1])
-        for hoja in hojas_procesadas[:1]:
+        st.write(hojas_procesadas[1:])
+        for hoja in hojas_procesadas[1:]:
             n+=1
             df_consolidado = pd.merge(df_consolidado, hoja, on=['HOLEID', 'From', 'To'], how='outer')
             with pd.ExcelWriter(output, engine="openpyxl") as writer:
